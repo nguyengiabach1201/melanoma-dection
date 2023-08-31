638f64f9629a75d10d709695e79063b50da9bb87
nguyengiabach1201 <108968549+nguyengiabach1201@users.noreply.github.com>
2023-08-31 11:50:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import numpy as np
+import tensorflow as tf
 
+def predict(model, img_path, class_names):
+    img = tf.keras.preprocessing.image.load_img(img_path, target_size=(256, 256))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * np.max(predictions[0]), 2)
+
+    return predicted_class, confidence
+
+model = tf.keras.models.load_model('C:/Users/Acer/Downloads/model1')
+
+class_names = ['benign', 'malignant']
+
+predicted_class, confidence = predict(model,'C:/Users/Acer/Downloads/Melanoma.jpg',class_names)
+print(predicted_class, confidence)
