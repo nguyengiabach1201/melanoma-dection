36e380dac55ff65a1a45aeebd3f83b122a0889af
nguyengiabach1201 <108968549+nguyengiabach1201@users.noreply.github.com>
2023-08-31 15:41:55 +0700

    Update training (on kaggle).py

--- training (on kaggle).py
+++ training (on kaggle).py
@@ -1,1 +1,121 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
 
+BATCH_SIZE = 32
+IMAGE_SIZE = 256
+CHANNELS=3
+EPOCHS=1
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "/kaggle/input/melanoma-skin-cancer-dataset-of-10000-images/melanoma_cancer_dataset/train",
+    seed=123,
+    shuffle=True,
+    image_size=(IMAGE_SIZE,IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names = dataset.class_names
+
+def get_dataset_partitions_tf(ds, train_split=0.9, val_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + val_split) == 1
+
+    ds_size = len(ds)
+
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+
+    train_ds = ds.take(train_size)
+    val_ds = ds.skip(train_size).take(val_size)
+
+    return train_ds, val_ds
+
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/kaggle/input/melanoma-skin-cancer-dataset-of-10000-images/melanoma_cancer_dataset/test",
+    seed=123,
+    shuffle=True,
+    image_size=(IMAGE_SIZE,IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+train_ds, val_ds = get_dataset_partitions_tf(dataset)
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+from tensorflow.keras import models, layers
+resize_and_rescale = tf.keras.Sequential([
+  layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+  layers.experimental.preprocessing.Rescaling(1./255),
+])
+
+data_augmentation = tf.keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+    layers.experimental.preprocessing.RandomRotation(0.2),
+    layers.experimental.preprocessing.RandomZoom(0.2),
+    layers.experimental.preprocessing.RandomContrast(0.2),
+])
+
+train_ds = train_ds.map(
+    lambda x, y: (data_augmentation(x, training=True), y)
+).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+CHANNELS = 3
+
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 2
+
+model = models.Sequential([
+    resize_and_rescale,
+    layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),  # Add dropout layer after the first convolutional layer
+    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),  # Add dropout layer after the third convolutional layer
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),  # Add dropout layer after the last convolutional layer
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.5),  # Add dropout layer after the dense layer
+    layers.Dense(n_classes, activation='softmax'),
+])
+
+model.build(input_shape=input_shape)
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    restore_best_weights=True
+)
+
+# Train the model
+training = model.fit(
+    train_ds,
+    batch_size=BATCH_SIZE,
+    validation_data=val_ds,
+    verbose=1,
+    epochs=EPOCHS,
+    callbacks=[early_stopping]
+)
+
+model_version = 1
+model.save("/kaggle/working/model1")
